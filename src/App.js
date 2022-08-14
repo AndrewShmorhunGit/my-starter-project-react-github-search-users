b3eca6f0449770d74c05665305ee5db198f5e603
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-14 23:55:24 +0300

    Route Error component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact={true}>
-      <Dashboard></Dashboard>
-      </Route>
-      <Route path="/login">
-      <Login></Login>
-      </Route>
-      {/* <Error /> */}
+      <Switch>
+        <Route path="/" exact={true}>
+          <Dashboard></Dashboard>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
     </Router>
   );
 }
