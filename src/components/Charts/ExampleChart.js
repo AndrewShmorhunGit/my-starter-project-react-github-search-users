1c5b28dcb22d6511da7baa13d1ba60060548779a
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-15 15:38:15 +0300

    repos and charts start

--- src/components/Charts/ExampleChart.js
+++ src/components/Charts/ExampleChart.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import FusionCharts from "fusioncharts";
+import charts from "fusioncharts/fusioncharts.charts";
+import ReactFusioncharts from "react-fusioncharts";
 
-const ExampleChart = () => {
-  return <div>chart</div>;
+// Resolves charts dependancy
+charts(FusionCharts);
+
+const dataSource = {
+  chart: {
+    caption: "Countries With Most Oil Reserves [2017-18]",
+    subcaption: "In MMbbl = One Million barrels",
+    xaxisname: "Country",
+    yaxisname: "Reserves (MMbbl)",
+    numbersuffix: "K",
+    theme: "fusion",
+  },
+  data: [
+    {
+      label: "Venezuela",
+      value: "290",
+    },
+    {
+      label: "Saudi",
+      value: "260",
+    },
+    {
+      label: "Canada",
+      value: "180",
+    },
+    {
+      label: "Iran",
+      value: "140",
+    },
+    {
+      label: "Russia",
+      value: "115",
+    },
+    {
+      label: "UAE",
+      value: "100",
+    },
+    {
+      label: "US",
+      value: "30",
+    },
+    {
+      label: "China",
+      value: "30",
+    },
+  ],
 };
 
-export default ExampleChart;
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <ReactFusioncharts
+        type="column2d"
+        width="100%"
+        height="100%"
+        dataFormat="JSON"
+        dataSource={dataSource}
+      />
+    );
+  }
+}
